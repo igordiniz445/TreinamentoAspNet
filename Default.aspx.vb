3e34aa573ff72bb6ba45b286d6f2e80e3d2a5407
Igor Diniz <igor.comp.ufla@gmail.com>
2020-10-29 08:48:19 -0300

    Sql test

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -1,7 +1,22 @@
-﻿Public Class _Default
+﻿Imports System.Data.SqlClient
+Public Class _Default
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim myConn As SqlConnection
+        Dim myCmd As SqlCommand
+        Dim myReader As SqlDataReader
+        Dim results As String
+
+        myConn = New SqlConnection("Initial Catalog=Treinamento;" & _
+                   "Data Source=localhost;Integrated Security=SSPI;")
+
+        myCmd = myConn.CreateCommand
+        myCmd.CommandText = "SELECT * FROM Carros"
+
+        'Open the connection.
+        myConn.Open()
+
 
     End Sub
 
